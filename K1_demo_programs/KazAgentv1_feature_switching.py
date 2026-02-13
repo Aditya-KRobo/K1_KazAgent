6c2c18527a20a22cb88c23ad7e7108317d34895a
Aditya-KRobo <aditya.prakash@kazrobotics.com.au>
2026-02-13 14:32:21 +1100

    added vision and kazagent switching agent

---
+++ K1_demo_programs/KazAgentv1_feature_switching.py
@@ -0,0 +1,65 @@
+from evdev import InputDevice, categorize, ecodes
+
+# from inputs import get_gamepad
+
+from booster_robotics_sdk_python import (
+    DanceId,
+    WholeBodyDanceId,
+    B1LocoClient,
+    ChannelFactory,
+    RobotMode,
+    Position,
+    Orientation,
+    Posture,
+    GetModeResponse,
+    Quaternion,
+    Frame,
+    Transform,
+)
+import sys, time, random
+
+
+def main():
+
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} networkInterface")
+        sys.exit(-1)
+
+    ChannelFactory.Instance().Init(0, sys.argv[1])
+
+    client = B1LocoClient()
+    client.Init()
+    res = 0
+
+    res = client.ChangeMode(RobotMode.kCustom)
+    # res = client.GetUpWithMode(RobotMode.kWalking)
+    res = client.EnterWBCGait()
+
+    # Find the gamepad device file (e.g., /dev/input/eventX)
+    gamepad = InputDevice("/dev/input/js0")
+    print(gamepad)
+
+    # while True:
+    # events = get_gamepad()
+    # for event in events:
+    #     print(event.ev_type, event.code, event.state)
+
+
+    for event in gamepad.read_loop():
+        if event.type == ecodes.EV_KEY:
+            print(categorize(event))  # Button presses
+            # res = client.PlaySound("/home/booster/Workspace/K1_Custom_Behaviors/K1_test_programs/res/Michael Jackson - Billie Jean(2).mp3")
+            # res = client.Dance(DanceId.kNewYear)
+            print("Button event detected.")
+        elif event.type == ecodes.EV_ABS:
+            print(categorize(event))  # Stick movements
+            # res = client.StopSound()
+            # res = client.Dance(DanceId.kUltramanGuesture)
+            print("Stick movement detected.")
+
+    if res != 0:
+        print(f"Request failed: error = {res}")
+
+
+if __name__ == "__main__":
+    main()
