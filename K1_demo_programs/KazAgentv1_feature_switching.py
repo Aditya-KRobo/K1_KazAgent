430a5c43ce90924c7fdc5253f4402ac325d947e1
Booster <booster@boosterobotics.com>
2026-02-16 13:54:55 +0800

    feature switching keymapi update

--- K1_demo_programs/KazAgentv1_feature_switching.py
+++ K1_demo_programs/KazAgentv1_feature_switching.py
@@ -1,3 +1,22 @@
+But_A = "BTN_EAST"
+But_B = "BTN_C"
+But_X = "BTN_SOUTH"
+But_Y = "BTN_NORTH"
+But_LB = "BTN_WEST"
+But_RB = "BTN_Z"
+But_LT = "ABS_RX"
+But_RT = "ABS_RY"
+But_Min = "BTN_TL2"
+But_Plu = "BTN_TR2"
+D_X = "ABS_HAT0X"
+D_Y = "ABS_HAT0Y"
+But_START = "BTN_START"
+But_SELECT = "BTN_SELECT"
+But_O = "BTN_THUMBL"
+But_HOME = "BTN_MODE"
+
+Button_values = {"BTN_EAST": 0, "BTN_C": 0, "BTN_SOUTH": 0, "BTN_NORTH": 0, "BTN_WEST": 0, "BTN_Z": 0, "ABS_RX": 0, "ABS_RY": 0, "BTN_TL2": 0, "BTN_TR2": 0, "ABS_HAT0X": 0, "ABS_HAT0Y": 0, "BTN_START": 0, "BTN_SELECT": 0, "BTN_THUMBL": 0, "BTN_MODE": 15}
+
 # from evdev import InputDevice, categorize, ecodes
 
 from inputs import get_gamepad
@@ -38,12 +57,13 @@ def main():
     while True:
         events = get_gamepad()
         for event in events:
+            # pass
             print(f'{event.ev_type} | {event.code} | {event.state}')
-        if event.code == "" and event.state == 1:
+        if event.code == But_LB and event.state == 1:
             print("Requested Sit-down")
-        elif event.code == "" and event.state == 1:
+        elif event.code == But_RB and event.state == 1:
             print("Requested Stand-up")
-        elif event.code == "" and event.state == 1:
+        elif event.code == But_Min and event.state == 1 :
             print("Requested Dance")
 
     if res != 0:
